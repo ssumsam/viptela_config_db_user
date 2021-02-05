092a9180a50e49fa684de4111aae9f30a24747bd
ssumsam <35752267+ssumsam@users.noreply.github.com>
2021-02-05 11:35:11 +1100

    Update update_db_user.py

--- update_db_user.py
+++ update_db_user.py
@@ -1,16 +1,19 @@
-import pexpect
 import sys
+import pexpect
 from os import environ
 from pprint import pprint
 
 
 _VMANAGE_PROMPT = '#'
+_NAME_COLON_STRING = 'name:'
+_PASSWORD_COLON_STRING = 'assword:'
 
 
 def main():
     output = []
     vmanage_list = set(sys.argv[1:])
     if not vmanage_list:
+        print('ERROR - No input devices!!!')
         return output
     try:
         vmanage_user = environ['VMANAGE_USER']
@@ -23,16 +26,17 @@ def main():
         output.append('Missing one or more mandatory environment variables')
         print(output)
         return output
+    output = []
 
     for host in vmanage_list:
         child = pexpect.spawn('ssh {}@{}'.format(vmanage_user, host))
         try:
-            child.expect(['assword'])
+            child.expect([_PASSWORD_COLON_STRING])
         except (pexpect.EOF, pexpect.TIMEOUT):
             output.append('Unable to connect to {}'.format(host))
             child.close()
             continue
-
+	# if 'assword' in child.after
         child.sendline(vmanage_password)
         child.expect([_VMANAGE_PROMPT, 'assword'])
         if not child.after == _VMANAGE_PROMPT:
@@ -41,13 +45,13 @@ def main():
             continue
         # VMANAGE_PROMPT has been found.
         child.sendline('request nms configuration-db update-admin-user')
-        child.expect('name:')
+        child.expect(_NAME_COLON_STRING)
         child.sendline(old_db_user)
-        child.expect('assword:')
+        child.expect(_PASSWORD_COLON_STRING)
         child.sendline(old_db_password)
-        child.expect('name:')
+        child.expect(_NAME_COLON_STRING)
         child.sendline(new_db_user)
-        child.expect('assword:')
+        child.expect(_PASSWORD_COLON_STRING)
         child.sendline(new_db_password)
         child.expect(_VMANAGE_PROMPT)
         if child.after == _VMANAGE_PROMPT:
